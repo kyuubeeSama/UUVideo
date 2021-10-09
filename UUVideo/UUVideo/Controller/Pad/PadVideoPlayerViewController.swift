b67a1f0c266b23f1cb04cad891747e6b1923d893
kyuubeeSama <yshjbw@163.com>
2021-10-09 17:08:19 +0800

    pad 播放器界面

---
+++ UUVideo/UUVideo/Controller/Pad/PadVideoPlayerViewController.swift
@@ -0,0 +1,37 @@
+//
+//  PadVideoPlayerViewController.swift
+//  UUVideo
+//
+//  Created by Galaxy on 2021/10/9.
+//  Copyright © 2021 qykj. All rights reserved.
+//
+
+// 界面分三部分，右侧为推荐视频，单竖排. 左上视频播放地址。 左下，视频名称，视频介绍，剧集选择
+
+import UIKit
+
+class PadVideoPlayerViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK:视频播放器
+    
+    // MARK:底部内容
+
+    //MARK:左侧推荐视频
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
